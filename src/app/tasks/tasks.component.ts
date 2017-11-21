ed52ad7ff30291139701434bb3e9fb86b176dbb1
Otavio Henrique <otavio.henrique@hotmail.com>
2017-11-21 21:05:53 -0200

    Creating tasks component

---
+++ src/app/tasks/tasks.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+
+export class TasksComponent implements OnInit {
+  ngOnInit() {
+
+  }
+}
