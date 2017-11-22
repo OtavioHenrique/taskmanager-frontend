a449eea90b472c84ce64ef0754412eb6e409adce
Otavio Henrique <otavio.henrique@hotmail.com>
2017-11-22 21:37:21 -0200

    Moving model task to correct path

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-const TASKS: Array<any> = [
+import { Task } from './shared/task.model';
+
+const TASKS: Array<Task> = [
   { id: 1, title: "Task 1" },
   { id: 2, title: "Task 2" },
   { id: 3, title: "Task 3" },
